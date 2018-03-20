76d2c178feb4e234c93437a6a0dbd9f4db8a7634
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-20 18:08:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end 
   
   post '/piglatinize' do 
-    @piglatinized_text = PigLatinizer.new(params[:user_phrase])
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :pig_latin
   end 
   
